21fa7d113aa2d3ce8eeea0e212aede024ef981b6
anngreen <anngreen@qq.com>
2023-01-28 14:32:53 +0800

    todo

---
+++ src/components/DropDownMenu.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container" v-if="isvisual">
+        <div class="dropdown">
+            <div class="dropdown-item" @click="showAllOnclick">显示全部</div>
+            <div class="dropdown-item" @click="showCompleteOnclick">显示已完成</div>
+            <div class="dropdown-item" @click="hideCompleteOnclick">隐藏已完成</div>
+            <div class="dropdown-item" @click="changeBgImgOnclick">更换随机背景</div>
+        </div>    
+    </div>        
+</template>
+
+<script>
+export default {
+    name:"DropDownMenu",
+    props:{
+        isvisual:Boolean
+    },
+    methods:{
+        showAllOnclick(){
+            this.$emit('close')
+            this.$bus.$emit('showAllItems')
+        },
+        showCompleteOnclick(){
+            this.$emit('close')
+            this.$bus.$emit('showCompleteItems')
+        },
+        hideCompleteOnclick(){
+            this.$emit('close')
+            this.$bus.$emit('hideCompleteItems')
+        },
+        changeBgImgOnclick(){
+            this.$emit('close')
+            this.$bus.$emit('changeBg')
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+$color:rgba(0,0,0,0.608);
+    .container{
+        box-shadow: 5px 5px 10px 1px gainsboro;
+        border-radius: 5px;
+        background-color: #fff;
+        .dropdown{
+            display: flex;
+            flex-direction: column;
+            align-content: center;
+            .dropdown-item{
+                padding: 0px 10px;
+                line-height: 35px;
+                height:35px;
+                text-align: center;
+                color:$color;
+                user-select: none;
+                &:hover{
+                    cursor:pointer;
+                    background: #62a2e9;
+                    color:#fff
+                }
+                &:active{
+                    background: #fff;
+                    color:$color;
+                }
+            }
+        }
+    }
+</style>
---
+++ src/components/TFooter.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="container">
+        <div class="box">
+            <label id="add-icon" for="task">+</label>
+            <input type="text" 
+            id="task" 
+            v-model="task_content" 
+            placeholder="添加待办"
+            @keyup.enter="addTodo"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TFooter",
+    props:{
+        new_id:Number
+    },
+    data(){
+        return {
+        task_content:""
+        }
+    },
+    methods:{
+        addTodo(){
+            // console.log("输入完毕");
+            if(!this.task_content)
+            {
+                // 如果输入框为空
+                alert("任务内容不能为空！");
+                return
+            }
+            // 将新的待办事项添加到里列表中
+            const newTodo = {
+                tId:this.new_id + 1,
+                isComplete:false,
+                taskContent:this.task_content,
+            }
+            this.$emit('add_todo',newTodo)
+            this.task_content = ""
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+    $bgColor: rgba(0,0,0,0.589);
+    .container{
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        .box{
+            background-color: $bgColor;
+            box-sizing: border-box;
+            padding: 5px 20px;
+            border-radius: 5px;
+            width:86%;
+            height:50px;
+            display: flex;
+            flex-direction: row;
+            justify-content: start;
+            align-items: center;
+            #add-icon{
+                font-size: 32px;
+                font-weight: 500;
+                margin-right: 12px;
+            }
+            #task{
+                width:100%;
+                font-size: 18px;
+                font-weight: 500;
+                border: 0px;
+                background: transparent;
+                color:white;
+                &::placeholder{
+                    color: white;
+                }
+                &:focus{
+                    outline: none;
+                }
+            }
+        }
+    }
+</style>
